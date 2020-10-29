0b9fa52947396eea0bc7cc2247cd7e9b51ea1a63
Gabriel Alves <thecrawler11@gmail.com>
2020-10-29 01:32:47 -0300

    Error handling and support directory

--- src/com/thecrawler/Main.java
+++ src/com/thecrawler/Main.java
@@ -1,5 +1,6 @@
 package com.thecrawler;
 
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Locale;
 
@@ -7,15 +8,25 @@ public class Main {
 
     public static void main(String[] args) {
         Locale.setDefault(new Locale("en", "US"));
-
-        String path = args[0];
-        ArrayList<String> file = Util.readHandHistoryFile(path);
-        ArrayList<ArrayList<String>> handHistories = Util.extractHands(file);
-        Parser parser = new Parser();
-
-        for (ArrayList<String> handHistory: handHistories) {
-            parser.parse(handHistory);
-            parser.print();
+        // Iterate for all file/directory paths
+        for (String path: args) {
+            ArrayList<String> file;
+            try {
+                file = Util.readHandHistories(path);
+            } catch (FileNotFoundException e) {
+                System.err.println("Invalid file path: " + path);
+                continue;
+            }
+            ArrayList<ArrayList<String>> handHistories = Util.extractHands(file);
+            Parser parser = new Parser();
+            for (ArrayList<String> handHistory: handHistories) {
+                try {
+                    parser.parse(handHistory);
+                    parser.print();
+                } catch (Exception e) {
+                    System.err.println("Error in parse hand.\n");
+                }
+            }
         }
     }
 }
--- src/com/thecrawler/Util.java
+++ src/com/thecrawler/Util.java
@@ -8,47 +8,50 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Util {
-    public static ArrayList<String> readHandHistoryFile(String path) {
-        try {
-            File file = new File(path);
-            Scanner scanner = new Scanner(file);
-            ArrayList<String> lines = new ArrayList<>();
-            while (scanner.hasNextLine()) {
-                lines.add(scanner.nextLine());
-            }
-            return lines;
-        } catch (FileNotFoundException e) {
-            System.out.println("File not found!");
-            e.printStackTrace();
+    public static ArrayList<String> readHandHistories(String path) throws FileNotFoundException {
+        ArrayList<String> lines = new ArrayList<>();
+        File fileOrDirectory = new File(path);
+        File[] files;
+        if (fileOrDirectory.isDirectory()) {
+            // If is a directory, add all files
+            files = fileOrDirectory.listFiles();
+        } else {
+            // If is a file, add this file
+            files = new File[]{fileOrDirectory};
         }
-        return null;
+        if (files != null) {
+            for (File file : files) {
+                if (file.isFile()) {
+                    Scanner scanner = new Scanner(file);
+                    while (scanner.hasNextLine()) {
+                        lines.add(scanner.nextLine());
+                    }
+                }
+            }
+        }
+        return lines;
     }
 
     public static ArrayList<ArrayList<String>> extractHands(ArrayList<String> lines) {
-        Pattern firstLineHandHistory = Pattern.compile("PokerStars Hand #([0-9]*):  Hold\\'em No Limit \\(([$€£])([0-9\\.]*)\\/[$€£]([0-9\\.]*) .*\\) - (.*)");
+        Pattern firstLineHandHistory = Pattern.compile("PokerStars Hand #([0-9]*): {1,2}Hold'em No Limit \\(([$€£])([0-9.]*)/[$€£]([0-9.]*) .*\\) - (.*)");
         ArrayList<ArrayList<String>> hands = new ArrayList<>();
         ArrayList<String> aux = new ArrayList<>();
-        try {
-            for (String line: lines) {
-                Matcher matcher = firstLineHandHistory.matcher(line);
-                boolean isFirstLine = matcher.find();
-                if (isFirstLine) {
-                    if (aux.size() > 0) {
-                        hands.add(new ArrayList<>(aux));
-                        aux.clear();
-                    }
-                    aux = new ArrayList<>();
+        for (String line: lines) {
+            Matcher matcher = firstLineHandHistory.matcher(line);
+            boolean isFirstLine = matcher.find();
+            if (isFirstLine) {
+                if (aux.size() > 0) {
+                    hands.add(new ArrayList<>(aux));
+                    aux.clear();
                 }
-                if (line.length() > 0) {
-                    aux.add(line);
-                }
+                aux = new ArrayList<>();
             }
-            if (aux.size() > 0) {
-                hands.add(new ArrayList<>(aux));
+            if (line.length() > 0) {
+                aux.add(line);
             }
-        } catch (Exception e) {
-            System.out.println("Error in extract hands");
-            e.printStackTrace();
+        }
+        if (aux.size() > 0) {
+            hands.add(new ArrayList<>(aux));
         }
         return hands;
     }
